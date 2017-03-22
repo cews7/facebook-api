ea44753d06daa950677aac409a9602330ba4ec45
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-03-22 17:16:00 -0600

    add friends_count endpoint; add styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  get '/user_likes',             to: 'likes#index', as: :likes            
+  get '/user_friends',           to: 'friends#show', as: :friends
+  get '/user_likes',             to: 'likes#index',  as: :likes
   get '/auth/facebook/callback', to: 'sessions#create'
   delete '/logout',              to: 'sessions#destroy'
   get '/auth/facebook',          as: :login
